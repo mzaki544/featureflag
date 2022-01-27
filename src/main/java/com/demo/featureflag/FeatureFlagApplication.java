40b8b00fc873ae687e4cc23b6cdd50ae79f5f11a
m.zaki544@gmail.com <m.zaki544@gmail.com>
2022-01-27 13:10:21 +0200

    init

---
+++ src/main/java/com/demo/featureflag/FeatureFlagApplication.java
@@ -0,0 +1,13 @@
+package com.demo.featureflag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureFlagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/featureflag/FeatureFlagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.featureflag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureFlagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
